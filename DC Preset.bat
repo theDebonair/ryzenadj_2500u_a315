f7fce2e1a08e12a98611292861d2c7069840c5ed
Harshit Gupta <harshit4277448@live.com>
2022-07-30 14:44:00 +0530

    Update DC Preset.bat

--- DC Preset.bat
+++ DC Preset.bat
@@ -1,2 +1,2 @@
 @echo off
-%~dp0\ryzenadj.exe --stapm-limit=22563 --stapm-time=60 --fast-limit=28500 --slow-limit=23750 --slow-time=900 --vrm-current=36325 --vrmmax-current=50870 --max-performance --tctl-temp=65
+%~dp0\ryzenadj.exe --stapm-limit=22560 --stapm-time=60 --fast-limit=28500 --slow-limit=23750 --slow-time=900 --vrm-current=36330 --vrmmax-current=50870 --max-performance --tctl-temp=65
